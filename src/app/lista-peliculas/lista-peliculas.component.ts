ee002a048e5d40906160f6ecd80265227011ea73
fabricastro <fabricastrook@gmail.com>
2023-05-22 23:45:24 -0300

    todos los filtros

--- src/app/lista-peliculas/lista-peliculas.component.ts
+++ src/app/lista-peliculas/lista-peliculas.component.ts
@@ -11,64 +11,87 @@ export class ListaPeliculasComponent implements OnInit {
       "id": 1,
       "title": "The Shawshank Redemption",
       "date": "2019",
-      "imagen": "https://i.ytimg.com/vi/19THOH_dvxg/movieposter_en.jpg"
+      "imagen": "https://i.ytimg.com/vi/19THOH_dvxg/movieposter_en.jpg",
+      "tipo": "Pelicula",
+      "genero":"Drama"
     },
     {
       "id": 2,
       "title": "Toy Story",
       "date": "2020",
-      "imagen": "https://www.efeeme.com/wp-content/uploads/2016/10/toy-story-16-10-16-b.jpg"
+      "imagen": "https://www.efeeme.com/wp-content/uploads/2016/10/toy-story-16-10-16-b.jpg",
+      "tipo": "Pelicula",
+      "genero":"Infantil"
     },
     {
       "id": 3,
       "title": "Breaking Bad",
       "date": "2015",
-      "imagen": "https://es.web.img3.acsta.net/pictures/18/04/04/22/52/3191575.jpg"
+      "imagen": "https://es.web.img3.acsta.net/pictures/18/04/04/22/52/3191575.jpg",
+      "tipo": "Serie",
+      "genero": "Accion",
     },
     {
       "id": 4,
       "title": "House of the dragon",
       "date": "2022",
-      "imagen": "https://images.cdn1.buscalibre.com/fit-in/360x360/38/24/38245503df8ecba45e5682480faea927.jpg"
+      "imagen": "https://images.cdn1.buscalibre.com/fit-in/360x360/38/24/38245503df8ecba45e5682480faea927.jpg",
+      "tipo": "Serie",
+      "genero": "Ficcion",
     },
-    {
-      "id": 5,
+   {
+      "id": 1,
       "title": "The Shawshank Redemption",
       "date": "2019",
-      "imagen": "https://i.ytimg.com/vi/19THOH_dvxg/movieposter_en.jpg"
+      "imagen": "https://i.ytimg.com/vi/19THOH_dvxg/movieposter_en.jpg",
+      "tipo": "Pelicula",
+      "genero":"Drama"
     },
     {
-      "id": 6,
+      "id": 2,
       "title": "Toy Story",
       "date": "2020",
-      "imagen": "https://www.efeeme.com/wp-content/uploads/2016/10/toy-story-16-10-16-b.jpg"
+      "imagen": "https://www.efeeme.com/wp-content/uploads/2016/10/toy-story-16-10-16-b.jpg",
+      "tipo": "Pelicula",
+      "genero":"Infantil"
     },
     {
-      "id": 7,
+      "id": 3,
       "title": "Breaking Bad",
       "date": "2015",
-      "imagen": "https://es.web.img3.acsta.net/pictures/18/04/04/22/52/3191575.jpg"
+      "imagen": "https://es.web.img3.acsta.net/pictures/18/04/04/22/52/3191575.jpg",
+      "tipo": "Serie",
+      "genero": "Accion",
     },
     {
-      "id": 8,
+      "id": 4,
       "title": "House of the dragon",
       "date": "2022",
-      "imagen": "https://images.cdn1.buscalibre.com/fit-in/360x360/38/24/38245503df8ecba45e5682480faea927.jpg"
-    }
+      "imagen": "https://images.cdn1.buscalibre.com/fit-in/360x360/38/24/38245503df8ecba45e5682480faea927.jpg",
+      "tipo": "Serie",
+      "genero": "Ficcion",
+    },
   ];
 
-   filtroAnio: string = '';
+  filtroGenero: string = '';
+  filtroTipo: string = '';
+  filtroAnio: string = '';
   peliculasFiltradas: any[] = [];
+  mostrarMensajeNoCoincidencias: boolean = false;
 
   ngOnInit() {
     this.aplicarFiltro();
   }
 
-  aplicarFiltro() {
-    if (this.filtroAnio !== '') {
-      this.peliculasFiltradas = this.peliculas.filter(pelicula => pelicula.date === this.filtroAnio);
-    } else {
-      this.peliculasFiltradas = this.peliculas;
-    }
-  }
+ aplicarFiltro() {
+  this.peliculasFiltradas = this.peliculas.filter(pelicula => {
+    const cumpleFiltroAnio = this.filtroAnio === '' || pelicula.date === this.filtroAnio;
+    const cumpleFiltroGenero = this.filtroGenero === '' || pelicula.genero === this.filtroGenero;
+    const cumpleFiltroTipo = this.filtroTipo === '' || pelicula.tipo === this.filtroTipo;
+
+    return cumpleFiltroAnio && cumpleFiltroGenero && cumpleFiltroTipo;
+  });
+   this.mostrarMensajeNoCoincidencias = this.peliculasFiltradas.length === 0;
+}
+
 }
